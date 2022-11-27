4bc430d23644c4c612979cf2e2c5ca22d1672c15
selma <selmakurtovic@live.com>
2022-11-27 22:14:10 +0100

    izmijenjen html

---
+++ scripts/prisustvo.js
@@ -0,0 +1,19 @@
+let div = document.getElementById("divSadrzaj");
+//instanciranje
+let prisustvo = TabelaPrisustvo(div, {studenti: [{ime:"Neko",index:12345}, {ime:"Neko2",index:12347}
+,{ime:"Neko3",index:12348} ,{ime:"Neko4",index:12349},{ime:"Neko5",index:12350}
+
+], 
+prisustva:[
+ {sedmica:1,predavanja:1,vjezbe:1,index:12345}, {sedmica:1,predavanja:3,vjezbe:2,index:12347}, {sedmica:1,predavanja:3,vjezbe:2,index:12348},{sedmica:1,predavanja:3,vjezbe:2,index:12349},
+ {sedmica:2,predavanja:1,vjezbe:2,index:12345}, {sedmica:6,predavanja:2,vjezbe:1,index:12347},{sedmica:2,predavanja:3,vjezbe:2,index:12348},{sedmica:5,predavanja:3,vjezbe:2,index:12349},
+ {sedmica:3,predavanja:1,vjezbe:2,index:12345}, {sedmica:3,predavanja:2,vjezbe:1,index:12347},{sedmica:3,predavanja:1,vjezbe:2,index:12349},
+ {sedmica:4,predavanja:1,vjezbe:2,index:12345}, {sedmica:4,predavanja:2,vjezbe:1,index:12347},{sedmica:4,predavanja:3,vjezbe:2,index:12348},{sedmica:4,predavanja:2,vjezbe:2,index:12349},
+
+
+], 
+predmet:"WT", brojPredavanjaSedmicno:3, brojVjezbiSedmicno:2});
+
+//pozivanje metoda
+//prisustvo.sljedecaSedmica();
+//prisustvo.prethodnaSedmica();
