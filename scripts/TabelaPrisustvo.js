854d56c69566cfacb329ba2cf6dabf240f2f1111
selma <selmakurtovic@live.com>
2022-11-27 22:10:20 +0100

    pocetak

---
+++ scripts/TabelaPrisustvo.js
@@ -0,0 +1,19 @@
+
+ 
+
+let TabelaPrisustvo = function (divRef, podaci) {
+    //ovdje definisano da ne bude error kad podaci nisu validni
+    let sljedecaSedmica = function () {}
+    let prethodnaSedmica = function () {}
+
+   
+
+ return {
+        sljedecaSedmica: sljedecaSedmica,
+        prethodnaSedmica: prethodnaSedmica
+    }
+    
+   
+    
+   
+};
